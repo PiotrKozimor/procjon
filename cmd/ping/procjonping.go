fd26ce4e7f081a1d2d7534592fa831a45bb9f84b
Piotr Kozimor <p1996k@gmail.com>
2020-08-24 19:50:55 +0200

    feat: run new procjonping

--- cmd/ping/procjonping.go
+++ cmd/ping/procjonping.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/PiotrKozimor/procjon/procjonagent"
 	log "github.com/sirupsen/logrus"
-	"github.com/sparrc/go-ping"
 	"github.com/spf13/cobra"
 )
 
@@ -16,37 +15,17 @@ func init() {
 		TimestampFormat: time.Stamp})
 	log.SetOutput(os.Stderr)
 	log.SetLevel(log.InfoLevel)
-	procjonagent.RootCmd.Flags().StringVar(&host, "host", "google.com", "host to ping")
-	procjonagent.RootCmd.Flags().Int32Var(&pings, "pings", 3, "number of pings to send")
-	procjonagent.RootCmd.Flags().Int32Var(&pingInterval, "ping-interval", 1, "ping interval [s]")
 	procjonagent.RootCmd.Use = "procjonping"
 	procjonagent.RootCmd.Short = "procjonping is procjon agent"
-	procjonagent.RootCmd.Long = `Procjonping is procjon agent which ping given host with ping-interval. 
-Few pings are sent (number is set by "pings" flag). 
-It sends status "pinged" when at least one ping succeded and 
-status "unreachable" when all pings failed. Please note that service timeout 
-is automatically adjusted to match total ping time.`
+	procjonagent.RootCmd.Long = `Procjonping is procjon agent which just returns ok status.
+Can be used to monitor host it is running on (e.g. network connection and power status).`
 	procjonagent.RootCmd.Run = func(cmd *cobra.Command, args []string) {
 		l, err := log.ParseLevel(procjonagent.LogLevel)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		log.SetLevel(l)
-		pinger, err := ping.NewPinger(host)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		pinger.Count = int(pings)
-		pinger.Interval = time.Second * time.Duration(pingInterval)
-		pinger.Timeout = time.Second * time.Duration((pings+1)*pingInterval)
-		pinger.SetPrivileged(true)
-		minTimeout := (pingInterval+1+procjonagent.Period)*2 + 1
-		if procjonagent.Timeout < minTimeout {
-			procjonagent.Timeout = minTimeout
-		}
-		monitor := procjonagent.PingMonitor{
-			Pinger: *pinger,
-		}
+		monitor := procjonagent.PingMonitor{}
 		err = procjonagent.HandleMonitor(&monitor)
 		log.Fatalln(err)
 	}
